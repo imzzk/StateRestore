7575b0f51bce3086d5cb0108b08839af1a14cd5d
imzzk <zhangkun@cnblogs.com>
2013-06-26 11:12:54 +0800

    Initial Commit

---
+++ StateRestoreDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StateRestoreDemo
+//
+//  Created by zhang kun on 6/26/13.
+//  Copyright (c) 2013 zhang kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StateRestoreDemo/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  StateRestoreDemo
+//
+//  Created by zhang kun on 6/26/13.
+//  Copyright (c) 2013 zhang kun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
